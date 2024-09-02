b74b24bcac81fdbb134161076f21a9ef6cafe7d5
Aman Hossain <amanhossainmail@gmail.com>
2024-09-02 15:45:30 +0530

    Work Added

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
-from flask import Flask, redirect, url_for
+from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to my Flask App...."
+    return render_template('index.html')
 
 @app.route('/success/<int:score>')
 def success(score):
-    return "The Person has passed and the marks is "+ str(score)
+    res=""
+    if score>=50:
+        res="PASS"
+    else:
+        res='FAIL'
+    exp={'Score':score,'Result':res}
+    return render_template('result.html',result=exp)
+
 
 @app.route('/fail/<int:score>')
 def fail(score):
     return "The Person has failed and the marks is "+ str(score)
 
-@app.route('/result/<int:marks>')
-def result(marks):
+### Result checker
+@app.route('/results/<int:marks>')
+def results(marks):
     result=""
-    if marks > 50:
-        result = 'success'
-    else:
+    if marks<50:
         result='fail'
-    return redirect(url_for(result, score=marks))
+    else:
+        result='success'
+    return redirect(url_for(result,score=marks))
+
+### Result checker submit html page
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        py=float(request.form['py'])
+        data_science=float(request.form['datascience'])
+        total_score=(science+maths+py+data_science)/4
+
+    return redirect(url_for('success',score=total_score))
+
 
 if __name__=='__main__':
     app.run(debug=True)
